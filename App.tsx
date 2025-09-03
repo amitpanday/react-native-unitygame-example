5fe0092b4ac87576688e22e54bd86f3d55c8dde8
amitpanday <amitp381997@gmail.com>
2025-09-04 00:59:29 +0530

    code fix

--- App.tsx
+++ App.tsx
@@ -1,45 +1,111 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// import React, { useRef, useEffect } from 'react';
+//  // @ts-ignore
+// import UnityView from '@azesmway/react-native-unity';
+// import { View } from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+// interface IMessage {
+//   gameObject: string;
+//   methodName: string;
+//   message: string;
+// }
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+// const Unity = () => {
+//   const unityRef = useRef<UnityView>(null);
 
+//   useEffect(() => {
+//     if (unityRef?.current) {
+//       const message: IMessage = {
+//         gameObject: 'gameObject',
+//         methodName: 'methodName',
+//         message: 'message',
+//       };
+//       unityRef.current.postMessage(
+//         message.gameObject,
+//         message.methodName,
+//         message.message
+//       );
+//     }
+//   }, []);
+
+//   return (
+//     <View style={{ flex: 1 }}>
+//       <UnityView
+//         ref={unityRef}
+//         style={{ flex: 1 }}
+//         onUnityMessage={(result: { nativeEvent: { message: any; }; }) => {
+//           console.log('onUnityMessage', result.nativeEvent.message);
+//         }}
+//       />
+//     </View>
+//   );
+// };
+
+// export default Unity;
+
+
+
+// import React, { useRef, useEffect } from 'react';
+
+// import UnityView from '@azesmway/react-native-unity';
+// import { View } from 'react-native';
+
+// const Unity = () => {
+//   const unityRef = useRef <UnityView> (null);
+
+//   useEffect(() => {
+//     if (unityRef?.current) {
+//       const message = {
+//         gameObject: 'gameObject',
+//         methodName: 'methodName',
+//         message: 'message',
+//       };
+//       unityRef.current.postMessage(
+//         message.gameObject,
+//         message.methodName,
+//         message.message
+//       );
+//     }
+//   }, []);
+
+//   return (
+//     <View style={{ flex: 1 }}>
+//       <UnityView
+//         ref={unityRef}
+//         style={{ flex: 1 }}
+//         onUnityMessage={(result: { nativeEvent: { message: any; }; }) => {
+//           console.log('onUnityMessage', result.nativeEvent.message);
+//         }}
+//       />
+//     </View>
+//   );
+// };
+
+// export default Unity;
+
+
+
+import React from 'react';
+// @ts-ignore
+import UnityView from '@azesmway/react-native-unity';
+import { Text, View } from 'react-native';
+
+const UnityApp = () => {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
+    <View style={{ flex: 1, justifyContent:"center", alignContent:"center", alignItems:"center" }}>
+      <Text style={{color:"#000", fontSize:18}}>
+        Hello, it's running
+      </Text>
+      <UnityView
+        style={{
+          position: 'absolute',
+          height: '100%',
+          width: '100%',
+          top: 1,
+          bottom: 1
+        }}
       />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
+export default UnityApp;
